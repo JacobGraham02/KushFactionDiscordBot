4e6a9945c5cb308edd85fffd9323064da68ea5ea
Jacob Graham <jakegraham54@gmail.com>
2025-01-24 15:50:11 -0500

    Created a custom discord client class that extends the functionality of the native Client class which exists in discord.js; this will allow me to add commands and other custom things to the bot

--- server/src/Index.ts
+++ server/src/Index.ts
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+/*
+Imports for use with the discord.js library
+ */
+import CustomDiscordClient from "./utilities/CustomDiscordClient";
+import {Collection, GatewayIntentBits} from 'discord.js';
+
+const client: CustomDiscordClient = new CustomDiscordClient({
+       intents: [
+              GatewayIntentBits.Guilds,
+              GatewayIntentBits.GuildMessages,
+              GatewayIntentBits.MessageContent,
+              GatewayIntentBits.GuildMembers,
+              GatewayIntentBits.GuildPresences
+       ]
+});
+
+
--- server/src/utilities/CustomDiscordClient.ts
+++ server/src/utilities/CustomDiscordClient.ts
@@ -1,2 +1,14 @@
-import { Client, Collection } from 'discord.js';
+import {Client, ClientOptions, Collection} from 'discord.js';
+import {Command} from "../interfaces/Command";
+
+/*
+This class is used to create a custom Discord client that supports additional functionality that is not found in the traditional Discord.js Client class.
+ */
+export default class CustomDiscordClient extends Client {
+    discord_commands: Collection<string, Command>;
+    constructor(options: ClientOptions) {
+        super(options);
+        this.discord_commands = new Collection();
+    }
+}
 
